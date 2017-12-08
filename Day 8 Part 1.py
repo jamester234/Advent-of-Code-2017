1fa636ac981d9cb2a21947bec19770dc2c6b49f1
James Jiang <jamesjiang52@gmail.com>
2017-12-08 00:41:24 -0500

    Add files via upload

---
+++ Day 8 Part 1.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 00:06:18 2017
+
+@author: James Jiang
+"""
+
+all_lines = [line.rstrip('\n') for line in open('Day 8 Data.txt')]
+
+instructions = []
+for line in all_lines:
+    strings = line.split()
+    instructions.append(strings)
+    
+values_dict = {}
+
+for i in range(len(instructions)):
+    values_dict[instructions[i][0]] = 0
+
+for i in range(len(instructions)):
+    if instructions[i][-2] == '>':
+        if values_dict[instructions[i][-3]] > int(instructions[i][-1]):
+            if instructions[i][1] == 'inc':
+                values_dict[instructions[i][0]] += int(instructions[i][2])
+            else:
+                values_dict[instructions[i][0]] -= int(instructions[i][2])
+    if instructions[i][-2] == '<':
+        if values_dict[instructions[i][-3]] < int(instructions[i][-1]):
+            if instructions[i][1] == 'inc':
+                values_dict[instructions[i][0]] += int(instructions[i][2])
+            else:
+                values_dict[instructions[i][0]] -= int(instructions[i][2])
+    if instructions[i][-2] == '>=':
+        if values_dict[instructions[i][-3]] >= int(instructions[i][-1]):
+            if instructions[i][1] == 'inc':
+                values_dict[instructions[i][0]] += int(instructions[i][2])
+            else:
+                values_dict[instructions[i][0]] -= int(instructions[i][2])
+    if instructions[i][-2] == '<=':
+        if values_dict[instructions[i][-3]] <= int(instructions[i][-1]):
+            if instructions[i][1] == 'inc':
+                values_dict[instructions[i][0]] += int(instructions[i][2])
+            else:
+                values_dict[instructions[i][0]] -= int(instructions[i][2])
+    if instructions[i][-2] == '==':
+        if values_dict[instructions[i][-3]] == int(instructions[i][-1]):
+            if instructions[i][1] == 'inc':
+                values_dict[instructions[i][0]] += int(instructions[i][2])
+            else:
+                values_dict[instructions[i][0]] -= int(instructions[i][2])
+    if instructions[i][-2] == '!=':
+        if values_dict[instructions[i][-3]] != int(instructions[i][-1]):
+            if instructions[i][1] == 'inc':
+                values_dict[instructions[i][0]] += int(instructions[i][2])
+            else:
+                values_dict[instructions[i][0]] -= int(instructions[i][2])
+        
+maximum = values_dict[instructions[0][0]]
+for i in values_dict:
+    if values_dict[i] > maximum:
+        maximum = values_dict[i]
+
+print(maximum)    
+

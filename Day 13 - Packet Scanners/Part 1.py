7972dad5e9ccd7e9b214cc0362944e516fe2a900
James Jiang <jamesjiang52@gmail.com>
2017-12-13 21:12:27 -0500

    Shortened process input block

--- Day 13 - Packet Scanners/Part 1.py
+++ Day 13 - Packet Scanners/Part 1.py
@@ -16,18 +16,9 @@ all_lines = [line.rstrip('\n') for line in open('Data.txt')]
 
 ranges = {}
 
-for pair in all_lines:
-    chars = [i for i in pair]
-    depth_firewall = ''
-    range_firewall = ''
-    x = 0
-    while chars[x] != ':':
-        depth_firewall += chars[x]
-        x += 1
-    x += 1
-    for i in range(x, len(chars)):
-        range_firewall += chars[i]
-    ranges[int(depth_firewall)] = int(range_firewall)
+for i in all_lines:
+    pair = i.split(': ')
+    ranges[int(pair[0])] = int(pair[1])
 
 severity = 0
 

85d305c826c3611605ce18bc3c5ea0a6d24ab427
James Jiang <jamesjiang52@gmail.com>
2017-12-19 14:16:21 -0500

    Add part 1 to folder

--- Day 19 - A Series of Tubes/Part 1.py
+++ Day 19 - A Series of Tubes/Part 1.py
@@ -5,7 +5,7 @@ Created on Tue Dec 19 13:21:23 2017
 @author: James Jiang
 """
 
-all_lines = [line.rstrip('\n') for line in open('Day 19 Data.txt')]
+all_lines = [line.rstrip('\n') for line in open('Data.txt')]
 all_lines_chars = []
 
 letters = [i for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']

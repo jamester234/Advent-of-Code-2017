57098ceecb43a65c2b66534c7ca93892940bb27a
James Jiang <jamesjiang52@gmail.com>
2018-01-05 18:38:56 -0500

    Reduced factor search range upper limit from n/2 to sqrt(n)

--- Day 23 - Coprocessor Conflagration/Part 2.py
+++ Day 23 - Coprocessor Conflagration/Part 2.py
@@ -12,7 +12,7 @@ def is_prime(n):
         return True
     if n == 3:
         return True
-    for i in range(2, int(n/2) + 1):
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     else:
